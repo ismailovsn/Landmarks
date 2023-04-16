5f88a1dc3ab7ac4dd2a670463a24538b4bea26e0
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-04-16 10:08:59 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Саид-Насир Исмаилов on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
